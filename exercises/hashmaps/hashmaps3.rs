59051377f3fb902b64161525f9ed3561dd1967e7
x595036279 <75958139+x595036279@users.noreply.github.com>
2023-10-10 04:03:16 +0000

    完成: modules,hashmaps,quiz2

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -3,7 +3,7 @@
 // A list of scores (one per line) of a soccer match is given. Each line is of
 // the form : "<team_1_name>,<team_2_name>,<team_1_goals>,<team_2_goals>"
 // Example: England,France,4,2 (England scored 4 goals, France 2).
-//
+
 // You have to build a scores table containing the name of the team, goals the
 // team scored, and goals the team conceded. One approach to build the scores
 // table is to use a Hashmap. The solution is partially written to use a
@@ -14,7 +14,7 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 use std::collections::HashMap;
 
@@ -34,11 +34,26 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
+
         // TODO: Populate the scores table with details extracted from the
         // current line. Keep in mind that goals scored by team_1
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+
+        let team_1_team = scores.entry(team_1_name).or_insert(Team {
+            goals_scored: 0,
+            goals_conceded: 0,
+        });
+        team_1_team.goals_scored += team_1_score;
+        team_1_team.goals_conceded += team_2_score;
+
+        let team_2_team = scores.entry(team_2_name).or_insert(Team {
+            goals_scored: 0,
+            goals_conceded: 0,
+        });
+        team_2_team.goals_scored += team_2_score;
+        team_2_team.goals_conceded += team_1_score;
     }
     scores
 }
