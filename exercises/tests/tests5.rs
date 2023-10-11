585d3d449614c4af5c04cf434ef00c50f433333f
x595036279 <75958139+x595036279@users.noreply.github.com>
2023-10-11 13:42:38 +0000

    撤回test5

--- exercises/tests/tests5.rs
+++ exercises/tests/tests5.rs
@@ -22,17 +22,17 @@
 // Execute `rustlings hint tests5` or use the `hint` watch subcommand for a
 // hint.
 
-
+// I AM NOT DONE
 
 /// # Safety
 ///
 /// The `address` must contain a mutable reference to a valid `u32` value.
-unsafe fn modify_by_address(mut address: usize) {
+unsafe fn modify_by_address(address: usize) {
     // TODO: Fill your safety notice of the code block below to match your
     // code's behavior and the contract of this function. You may use the
     // comment of the test below as your format reference.
     unsafe {
-        address = 0xAABBCCDD;
+        todo!("Your code goes here")
     }
 }
 
@@ -46,6 +46,6 @@ mod tests {
         // SAFETY: The address is guaranteed to be valid and contains
         // a unique reference to a `u32` local variable.
         unsafe { modify_by_address(&mut t as *mut u32 as usize) };
-        //assert!(t == 0xAABBCCDD);
+        assert!(t == 0xAABBCCDD);
     }
 }
